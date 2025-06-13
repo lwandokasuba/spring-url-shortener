dc596f5315fc3c44ec61c6ac40d04ed3ed81c11d
lwandokasuba <lwandojapkas@gmail.com>
2025-06-13 18:42:21 +0200

    Add Redis configuration and caching support in ShortUrlService P.S. Needs optimizing why store everything as string by default

--- src/main/java/com/lwandokasuba/urlshortener/services/ShortUrlService.java
+++ src/main/java/com/lwandokasuba/urlshortener/services/ShortUrlService.java
@@ -3,6 +3,9 @@ package com.lwandokasuba.urlshortener.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 import com.lwandokasuba.urlshortener.models.ShortUrl;
@@ -13,14 +16,17 @@ public class ShortUrlService {
   @Autowired
   ShortUrlRepository shortUrlRepository;
 
+  @CacheEvict(cacheNames = "shortUrls", allEntries = true)
   public ShortUrl add(ShortUrl url) {
     return shortUrlRepository.save(url);
   }
 
+  @Cacheable(cacheNames = "shortUrls")
   public Iterable<ShortUrl> getAll() {
     return shortUrlRepository.findAll();
   }
 
+  @Cacheable(cacheNames = "shortUrl", key = "#id")
   public ShortUrl get(Integer id) {
     Optional<ShortUrl> url = shortUrlRepository.findById(id);
     if (url.isPresent()) {
@@ -30,11 +36,15 @@ public class ShortUrlService {
     return null;
   }
 
+  @Caching(evict = { @CacheEvict(cacheNames = "shortUrl", key = "#id"),
+      @CacheEvict(cacheNames = "shortUrls", allEntries = true) })
   public String delete(Integer id) {
     shortUrlRepository.deleteById(id);
     return new String("Deleted");
   }
 
+  @Caching(evict = { @CacheEvict(cacheNames = "shortUrl", key = "#id"),
+      @CacheEvict(cacheNames = "shortUrls", allEntries = true) })
   public ShortUrl update(Integer id, ShortUrl url) {
     ShortUrl urlToUpdate = shortUrlRepository.findById(id).get();
     if (url.getLongUrl() != null && url.getLongUrl().isEmpty() != true) {
